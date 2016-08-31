658136be1213c9ffb51ab01c5098f11c43b79222
Niko Solihin <nikosolihin@gmail.com>
2016-08-31 12:45:56 +0700

    set up sidebar

--- src/php/page.php
+++ src/php/page.php
@@ -8,7 +8,6 @@ $context['post'] = $post;
 $context['acf'] = get_fields();
 $context['sections'] = $context['acf']['sections'];
 
-//
 // // Get Sidebar
 // $context['inherit'] = ($context['acf']['inherit'] === 'true');
 // if ($context['inherit']) {
@@ -17,31 +16,34 @@ $context['sections'] = $context['acf']['sections'];
 // } else {
 // 	$context['sidebar_sections'] = $post->get_field('sidebar_sections');
 // }
-//
-// // Find root ID
-// if ($post->post_parent)	{
-// 	$ancestors = get_post_ancestors($post->id);
-// 	$id = $ancestors[ count($ancestors)-1 ];
-// } else {
-// 	$id = $post->id;
-// }
-// // and generate hierarchy
-// $args = array(
-//   'child_of' => $id,
-//   'echo' => false,
-//   'title_li' => null
-// );
-// $context['hierarchy'] = wp_list_pages($args);
-//
-// // Generate breadcrumb. Must be last.
-// $context['breadcrumb'] = array();
-// while( $post->get_parent ) {
-// 	$post = $post->get_parent();
-// 	array_push( $context['breadcrumb'], array(
-//     'title' => $post->title,
-//     'link' => $post->link
-//   ));
-// }
-// $context['breadcrumb'] = array_reverse( $context['breadcrumb'] );
+
+// Find root ID
+if ($post->post_parent)	{
+	$ancestors = get_post_ancestors($post->id);
+	$id = $ancestors[ count($ancestors)-1 ];
+} else {
+	$id = $post->id;
+}
+// and generate hierarchy
+$args = array(
+  'child_of' => $id,
+  'echo' => false,
+  'title_li' => null
+);
+$context['hierarchy'] = wp_list_pages($args);
+
+// Generate breadcrumb. Must be last.
+$context['breadcrumb'] = array();
+while( $post->get_parent ) {
+	$post = $post->get_parent();
+	array_push( $context['breadcrumb'], array(
+    'title' => $post->title,
+    'link' => $post->link
+  ));
+}
+$context['breadcrumb'] = array_reverse( $context['breadcrumb'] );
 
 Timber::render( 'page/single-page-base.twig' , $context );
+
+var_dump($id);
+var_dump($context['hierarchy']);
