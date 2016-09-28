d20f86d98e005034e4c2f571d56bd46fa8f72fcf
Niko Solihin <nikosolihin@gmail.com>
2016-09-28 13:31:58 +0700

    prep for people page template

--- src/php/lib/plugs.php
+++ src/php/lib/plugs.php
@@ -14,7 +14,7 @@ add_filter( "radio-buttons-for-taxonomies-no-term-news_topic", "__return_FALSE" 
 // Uncoment the action before pushing to prod
 // Use the GUI on dev since if using php GUI is disabled
 //==========================================================
-add_action( 'init', 'ac_custom_column_settings_5f331eb7' );
+// add_action( 'init', 'ac_custom_column_settings_5f331eb7' );
 function ac_custom_column_settings_5f331eb7() {
 	if ( function_exists( 'ac_register_columns' ) ) {
 		ac_register_columns( 'page', array(
---
+++ src/php/people-page.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template Name: Landing Page
+ * Description: Page template for landing page
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+$context['acf'] = get_fields();
+$context['sections'] = $context['acf']['sections'];
+$context['children'] = array();
+
+foreach ($post->get_children() as $child) {
+  $grandchildren = array();
+  if ( $context['acf']['landing_layout'] == "extensive" ) {
+    //Extensive. Fetch all children and grand children
+    foreach ($child->get_children() as $grandchild) {
+      array_push($grandchildren, array(
+        'title' => $grandchild->title(),
+        'link' => $grandchild->link()
+      ));
+    }
+  }
+  // And their teaser images.
+  array_push($context['children'], array(
+    'title' => $child->title(),
+    'link' => $child->link(),
+    'teaser' => $child->get_field('teaser'),
+    'image' => $child->get_field('image'),
+    'children' => $grandchildren
+  ));
+}
+
+Timber::render( 'page/landing-page-base.twig' , $context );
