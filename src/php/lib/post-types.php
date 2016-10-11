85961b5e1459e3e9554eeb9ad032965680884227
Niko Solihin <nikosolihin@gmail.com>
2016-10-11 14:07:06 +0700

    dev again

--- src/php/lib/post-types.php
+++ src/php/lib/post-types.php
@@ -183,6 +183,65 @@ function news_post_type() {
 }
 add_action( 'init', 'news_post_type', 0 );
 
+
+/**
+ * Register Person Post Type
+ */
+function person_post_type() {
+	$labels = array(
+		'name'                  => _x( 'People', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Person', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'People', 'text_domain' ),
+		'name_admin_bar'        => __( 'Person', 'text_domain' ),
+		'archives'              => __( 'People Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Person:', 'text_domain' ),
+		'all_items'             => __( 'All People', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Person', 'text_domain' ),
+		'add_new'               => __( 'New Person', 'text_domain' ),
+		'new_item'              => __( 'New Person', 'text_domain' ),
+		'edit_item'             => __( 'Edit Person', 'text_domain' ),
+		'update_item'           => __( 'Update Person', 'text_domain' ),
+		'view_item'             => __( 'View Person', 'text_domain' ),
+		'search_items'          => __( 'Search Person', 'text_domain' ),
+		'not_found'             => __( 'No person found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'No person found in trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Items list', 'text_domain' ),
+		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Person', 'text_domain' ),
+		'description'           => __( 'Custom post type for people', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title' ),
+		'taxonomies'            => array( 'role' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 102,
+		'menu_icon'             => 'dashicons-groups',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => false,
+		'can_export'            => true,
+		'has_archive'           => false,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => false, //Disable single page viewing
+		'rewrite'               => false,
+		'capability_type'       => 'page',
+		'show_in_rest'       		=> false,
+	);
+	register_post_type( 'person', $args );
+}
+add_action( 'init', 'person_post_type', 0 );
+
+
 /**
  * Register Media Post Type
  */
@@ -246,64 +305,6 @@ function media_post_type() {
 	register_post_type( 'media', $args );
 }
 add_action( 'init', 'media_post_type', 0 );
-
-/**
- * Register Person Post Type
- */
-function person_post_type() {
-	$labels = array(
-		'name'                  => _x( 'People', 'Post Type General Name', 'text_domain' ),
-		'singular_name'         => _x( 'Person', 'Post Type Singular Name', 'text_domain' ),
-		'menu_name'             => __( 'People', 'text_domain' ),
-		'name_admin_bar'        => __( 'Person', 'text_domain' ),
-		'archives'              => __( 'People Archives', 'text_domain' ),
-		'parent_item_colon'     => __( 'Parent Person:', 'text_domain' ),
-		'all_items'             => __( 'All People', 'text_domain' ),
-		'add_new_item'          => __( 'Add New Person', 'text_domain' ),
-		'add_new'               => __( 'New Person', 'text_domain' ),
-		'new_item'              => __( 'New Person', 'text_domain' ),
-		'edit_item'             => __( 'Edit Person', 'text_domain' ),
-		'update_item'           => __( 'Update Person', 'text_domain' ),
-		'view_item'             => __( 'View Person', 'text_domain' ),
-		'search_items'          => __( 'Search Person', 'text_domain' ),
-		'not_found'             => __( 'No person found', 'text_domain' ),
-		'not_found_in_trash'    => __( 'No person found in trash', 'text_domain' ),
-		'featured_image'        => __( 'Featured Image', 'text_domain' ),
-		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
-		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
-		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
-		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
-		'items_list'            => __( 'Items list', 'text_domain' ),
-		'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
-		'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
-	);
-	$args = array(
-		'label'                 => __( 'Person', 'text_domain' ),
-		'description'           => __( 'Custom post type for people', 'text_domain' ),
-		'labels'                => $labels,
-		'supports'              => array( 'title' ),
-		'taxonomies'            => array( 'role' ),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 102,
-		'menu_icon'             => 'dashicons-groups',
-		'show_in_admin_bar'     => true,
-		'show_in_nav_menus'     => false,
-		'can_export'            => true,
-		'has_archive'           => false,
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => false, //Disable single page viewing
-		'rewrite'               => false,
-		'capability_type'       => 'page',
-		'show_in_rest'       		=> false,
-	);
-	register_post_type( 'person', $args );
-}
-add_action( 'init', 'person_post_type', 0 );
-
 
 /**
  * Filters to enable %custom-taxonomy% in rewrites
--- src/php/lib/routes.php
+++ src/php/lib/routes.php
@@ -6,6 +6,6 @@ Routes::map('media', function($params){
   Routes::load('archive-media.php');
 });
 Routes::map('media/:type', function($params){
-  $url = 'http://localhost:3000/media/?utf8=%E2%9C%93&page=1&type=' . $params['type'];
+  $url = get_bloginfo('url').'/media/?utf8=%E2%9C%93&page=1&types=' . $params['type'];
   header( "Location: $url" );
 });
